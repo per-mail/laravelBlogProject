222a5c1eb2ebcecdbe02c7364c5dc938848b1bbc
per-mail <per-mail@mail.ru>
2023-04-10 21:02:49 +0700

    10/04

--- app/Http/Controllers/Admin/Category/CreateController.php
+++ app/Http/Controllers/Admin/Category/CreateController.php
@@ -13,3 +13,19 @@ class CreateController extends Controller
         return view ('admin.category.create');
     }
 }
+
+
+
+// namespace App\Http\Controllers\Admin\Category;
+
+// use App\Http\Controllers\Controller;
+
+
+// class CreateController extends Controller
+// {
+//     public function __invoke()    {
+
+// // метод view начинает поиск файла с папки view потом идёт папка admin папка category и файл create, точку между ними пишем вместо слэша
+//         return view ('admin.category.create');
+//     }
+// }
